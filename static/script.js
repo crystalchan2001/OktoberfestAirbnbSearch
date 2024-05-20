2d7823a24b701ef61ab1e87f95e01c353e0023b3
“Crystal <K19001568@kcl.ac.uk>
2024-05-20 15:02:03 +0100

    Validating input

--- static/script.js
+++ static/script.js
@@ -1,13 +1,25 @@
-const checkInDate = document.getElementById("checkin")
-const checkOutDate = document.getElementById("checkout")
+const checkInDate = document.getElementById("checkin");
+const checkOutDate = document.getElementById("checkout");
 
+checkInDate.addEventListener("change", validateInDate);
+checkOutDate.addEventListener("change", validateOutDate);
 
-checkOutDate.addEventListener("change", validateDate);
+function validateInDate() {
+    console.log("Checking the checkin date");
+    const checkIn = new Date(checkInDate.value);
+    const today = new Date();
 
-function validateDate() {
+    if (checkIn < today) {
+        alert("Your check in date should be later than today");
+        checkInDate.value= "";
+    }
+}
+
+function validateOutDate() {
+    console.log("Checking the checkout date");
     const checkIn = new Date(checkInDate.value);
     const checkOut = new Date(checkOutDate.value);
-
+ 
     if (checkOut <= checkIn) {
         alert("Check out date should be later than the check in date");
         checkOutDate.value = "";
