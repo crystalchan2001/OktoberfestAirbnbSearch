f32f089d96120cc278de6da2f31e574200338b28
“Crystal <K19001568@kcl.ac.uk>
2024-05-22 00:14:24 +0100

    Pass form input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import subprocess
 import datetime
@@ -7,41 +7,38 @@ import asyncio
 
 app = Flask(__name__)
 
-async def formGet():
-    variableNames = ["guests", "rooms", "checkin", "checkout", "bathrooms", "budget", "favouriteStr", "entireHomeStr", "frequency", "email"]
-    tasks = [str(request.form.get(var)) for var in variableNames]
-    results = await asyncio.gather(*tasks)
-    actualResults = []
-    for result in results:
-        actual = await result
-        print("Awaiting task...")
-        actualResults.append(actual)
-    return actualResults
+# created with the command 'python -c 'import secrets; print(secrets.token_hex())' as suggested by Flask documentation
+app.secret_key = b'95cfea064e940d1f09e3bd12b82eaa95a3cf59796af5cead02bba7686cd7249c'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://<username>:<password>@<server>:5432/<db_name>"
+db = SQLAlchemy(app)
+
+# async def formGet():
+#     variableNames = ["guests", "rooms", "checkin", "checkout", "bathrooms", "budget", "favouriteStr", "entireHomeStr", "frequency", "email"]
+#     tasks = [str(request.form.get(var)) for var in variableNames]
+#     results = await asyncio.gather(*tasks)
+#     actualResults = []
+#     for result in results:
+#         actual = await result
+#         print("Awaiting task...")
+#         actualResults.append(actual)
+#     return actualResults
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':        
-        timeStart = datetime.datetime.now()
-        try:
-            vars = asyncio.run(formGet())
-            print("[VARS] " + str(vars))
-        except:
-            print("THERE WAS AN ERROR")
-        # checkin = str(request.form.get('checkin'))
-        # checkout = str(request.form.get('checkout'))
-        # guests = str(request.form.get('guests'))
-        # rooms = str(request.form.get('rooms'))
-        # bathrooms = str(request.form.get('bathrooms'))
-        # budget = str(request.form.get('budget'))
-        # favouriteStr = str(request.form.get('favourite'))
-        # entireHomeStr = str(request.form.get('entire'))
-        # frequency = str(request.form.get('frequency'))
-        # email = str(request.form.get('email'))
+        checkin = str(request.form['checkin'])
+        checkout = str(request.form['checkout'])
+        guests = str(request.form['guests'])
+        rooms = str(request.form['rooms'])
+        bathrooms = str(request.form['bathrooms'])
+        budget = str(request.form['budget'])
+        favouriteStr = str(request.form['favourite'])
+        entireHomeStr = str(request.form['entire'])
+        frequency = str(request.form['frequency'])
+        email = str(request.form['email'])
+        session['formInput'] = [email, checkin, checkout, guests, rooms, bathrooms, budget, favouriteStr, entireHomeStr, frequency]
 
-        timeEnd = datetime.datetime.now()
-        timeTaken = timeEnd - timeStart
-        print(f"[TIME TAKEN] {timeTaken}")
-        
         # cmd = [
         #     'python',
         #     '/Users/chanc/Documents/2024/OktoberfestAccomodation/main.py',
@@ -56,34 +53,22 @@ def index():
         #     frequency,
         #     email
         # ]
-        # cmd = [
-        #     'python',
-        #     '/Users/chanc/Documents/2024/OktoberfestAccomodation/main.py',
-        #     vars[0],
-        #     vars[1],
-        #     vars[2],
-        #     vars[3],
-        #     vars[4],
-        #     vars[5],
-        #     vars[6],
-        #     vars[7],
-        #     vars[8],
-        #     vars[9]
-        # ]
-
-        # print(f"[CALLING CMDLINE] {cmd}")
 
         try:
             # subprocess.run(cmd, check=True, capture_output=True)
             return redirect(url_for('thanks'))
         except subprocess.CalledProcessError as e:
             return "Error executing code: " + str(e)
-        
-    return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 @app.route('/thanks')
 def thanks():
-    return render_template('thanks.html')
+    formInput = session['formInput']
+    return render_template('thanks.html', formIn=formInput)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 if __name__ == '__main__':
     app.run(debug=True)
